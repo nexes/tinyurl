5fd1645112578fd58c5b4f3fb6a9ed1c6193b8d8
Joe Berria <joeberria@gmail.com>
2017-10-01 13:19:35 -0700

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3
+ENV PYTHONUNBUFFERED 1
+
+RUN mkdir /code
+WORKDIR /code
+
+ADD . /code
+RUN echo "docker file run" &&\
+    pip install -r requirements.txt
---
+++ django-migrations.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# waiting for postgresql service
+
+cmd="$@"
+
+# # until psql -h "tinyurldb" -p "5432"  -c "\l"; do
+# while ! curl -v -X GET 'tinyurldb:5432/postgres'; do
+#     >&2 echo "Connecting to tinyurldb postgres database"
+#     sleep 2
+# done
+
+sleep 5
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+exec $cmd
