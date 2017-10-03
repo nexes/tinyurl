70b78cdb24b8472186f29eac20468ff2217e0284
Joe Berria <joeberria@gmail.com>
2017-10-02 22:43:44 -0700

    update to migrations

---
+++ shortner/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from shortner.views import (
+    CreateURL,
+    Expiration,
+    Usage
+)
+
+
+app_name = 'shortner'
+
+urlpatterns = [
+    url(r'^create/$', CreateURL.as_view(), name='create'),
+    url(r'^expiration/$', Expiration.as_view(), name='set-expiration'),
+    url(r'^expiration/(?P<url_hash>[a-zA-Z0-9]+)/$', Expiration.as_view(), name='get-expiration'),
+    url(r'^usage/$', Usage.as_view(), name='set-usage'),
+    url(r'^usage/(?P<url_hash>[a-zA-Z0-9]+)/$', Usage.as_view(), name='get-usage')
+]
